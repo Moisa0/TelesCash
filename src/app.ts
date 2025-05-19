cecc4b4311ca8a7f5a52dcdb2c9a696afab28414
Moisa <telesmoises8@gmail.com>
2025-05-19 02:29:27 -0300

    configuração do docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import express from 'express';
+import {PrismaClient} from '@prisma/client'
 
 export const app = express()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data:{
+        name:"moises",
+        email:"teles@gmail.com"
+        
+
+    }
+})
