685866a55f7a69b4c205a13ab3901cec240bca04
Moisa <telesmoises8@gmail.com>
2025-05-23 01:22:21 -0300

    iniciando criação de usuario

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,33 @@
+
 import express from 'express';
-import {PrismaClient} from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'; 
+import { AnyARecord } from 'dns';
 
 export const app = express()
 
-const prisma = new PrismaClient()
+app.use(express.json())
 
-prisma.user.create({
-    data:{
-        name:"moises",
-        email:"teles@gmail.com"
-        
 
-    }
+
+app.post('/users', async (req:any,res:any)=>{
+    const registerBodySchema = z.object({
+        name:z.string(),
+        email: z.string().email(),
+        password :z.string().min(6),
+        wallet :z.number()
+    })
+    console.log("Dados recebidos:", req.body)
+    const {name, email, password, wallet} = registerBodySchema.parse(req.body)
+
+    await prisma.user.create({
+        data:{
+            name,
+            email,
+            password_hash: password,
+            wallet
+        }
+    })
+
+    return res.status(201).send()
 })
