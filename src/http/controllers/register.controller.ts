b3ab98b7af5700c7543984510f4c9cd53fc85d71
Moisa <telesmoises8@gmail.com>
2025-05-27 02:07:21 -0300

    hash de senha e validação de email

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
+import { hash } from 'bcryptjs'
 
 
 
@@ -14,11 +15,24 @@ export async function register(req:Request, res:Response){
     console.log("Dados recebidos:", req.body)
     const {name, email, password, wallet} = registerBodySchema.parse(req.body)
 
+
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where:{
+            email,
+        }
+    })
+
+    if(userWithSameEmail){
+        return res.status(409).send('O email já está registrado')
+    }
+
     await prisma.user.create({
         data:{
             name,
             email,
-            password_hash: password,
+            password_hash,
             wallet
         }
     })
